28f9701289cdf38730c3f9bee69ab2b6a3700a13
Alonso Herrera <alonso.bkn.098@gmail.com>
2024-06-26 03:25:02 -0400

    FUNCIONA!!!!!!!!!!!!!!!!

---
+++ Server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { PythonShell } = require('python-shell');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post('/run-python', (req, res) => {
+    let { pythonCode } = req.body;
+    pythonCode = pythonCode + "print(num)";
+    console.log("codigo:\n",pythonCode);
+
+
+    PythonShell.runString(pythonCode, null, (err, results) => {
+        if (err) {return res.status(500).json({ error: err.message });}
+        console.log("Error:",err);
+        res.json({ results });
+        console.log("Resultado:",results);
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+console.log(`Port: ${PORT}`);
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
--- react-app/src/HUs/HU01.jsx
+++ react-app/src/HUs/HU01.jsx
@@ -1,42 +1,45 @@
-import { useEffect } from 'react';
+import { useEffect} from 'react';
 import * as Blockly from 'blockly';
+import { blocks } from './blocks/text';
+import { forBlock } from './generators/javascript';
 import { javascriptGenerator } from 'blockly/javascript';
-import { /*save,*/ load } from './serialization';
+import { pythonGenerator } from 'blockly/python';
+import { save, load } from './serialization';
+import { toolbox } from './toolbox';
 import './index.css';
-import Interpreter from 'js-interpreter';
-import {initInterpreter} from './Interpreter.js';
+import {Container,  Typography } from '@mui/material';
 
-import { toolboxXML, Blocks } from './Blocks.js';
-// import Maze from './Maze.jsx';
+import UsePython from './Python/RunPyCode';
+
+
 
 export default function HU01() {
+  
+  
   useEffect(() => {
-    const blocklyDiv = document.getElementById('blocklyDiv');
     // Registrar los bloques y generadores con Blockly
-    Blocks();
+    Blockly.common.defineBlocks(blocks);
+    Object.assign(javascriptGenerator.forBlock, forBlock);
 
     // Configurar elementos de la UI y inyectar Blockly
-    
     const codeDiv = document.getElementById('generatedCode').firstChild;
     const outputDiv = document.getElementById('output');
-    const ws = Blockly.inject(blocklyDiv, { toolbox : toolboxXML, trashcan: true});
-    javascriptGenerator.addReservedWords('moveForward,moveBackward,turnRight,turnLeft,isPathForward,isPathRight,isPathBackward,isPathLeft');
+    const blocklyDiv = document.getElementById('blocklyDiv');
+    const ws = Blockly.inject(blocklyDiv, { toolbox });
 
     // Función para resetear los divs de código y output, mostrar el código generado y ejecutarlo
-    const runCode = () => {
-
-      const code = getCode(ws);
-      console.log("Codigo generado:\n",code)
-      const code1 = javascriptGenerator.workspaceToCode(ws);
-      console.log("Codigo generado 1:\n",code1)
-      const interpreter = new Interpreter(code1, initInterpreter);
-    
-    
-      codeDiv.innerText = code;
-
-      outputDiv.innerHTML = '';
-
-      eval(code1);
+    const runCode = async () => {
+      // const code = javascriptGenerator.workspaceToCode(ws);
+      const codePy = pythonGenerator.workspaceToCode(ws);
+      codeDiv.innerText = codePy;
+      try {
+        const codigo = await UsePython(codePy);
+        if (codigo !== undefined) {
+          outputDiv.innerHTML = codigo;
+        }
+      } catch (error) {
+        console.error('Error executing Python code:', error);
+      }
     };
 
     // Cargar el estado inicial desde el almacenamiento y ejecutar el código
@@ -46,13 +49,15 @@ export default function HU01() {
     // Guardar los cambios en el almacenamiento cada vez que el workspace cambie de estado
     ws.addChangeListener((e) => {
       if (e.isUiEvent) return;
-      // save(ws);
+      save(ws);
     });
 
     // Ejecutar el código cada vez que el workspace cambie significativamente
     ws.addChangeListener((e) => {
       if (
-        e.isUiEvent || e.type == Blockly.Events.FINISHED_LOADING || ws.isDragging()
+        e.isUiEvent ||
+        e.type == Blockly.Events.FINISHED_LOADING ||
+        ws.isDragging()
       ) {
         return;
       }
@@ -62,108 +67,12 @@ export default function HU01() {
 
   return (
     <div id="pageContainer">
-      <div id="outputPane">
-        {/* <Maze/> */}
+      <Container id="outputPane">
         <pre id="generatedCode"><code></code></pre>
-        <div id="output"></div>
-      </div>
-      <div id="blocklyDiv" style={{ height: '480px', width: '600px' }}></div>
+        <Typography variant="h6">Console:</Typography>
+        <Container id="output" ></Container>
+      </Container>
+      <div id="blocklyDiv"/>
     </div>
   );
 }
-
-/**
- * Get the user's code (XML or JS) from the editor (Blockly or ACE).
- * @returns {string} XML or JS code.
- */
-function getCode (ws) {
-  let text;
-    // Blockly editor.
-    const xml = Blockly.Xml.workspaceToDom(ws, true);
-    // Remove x/y coordinates from XML if there's only one block stack. There's no reason to store this, removing it helps with anonymity.
-    if (ws.getTopBlocks(false).length === 1 && xml.querySelector) {
-      const block = xml.querySelector('block');
-      if (block) {
-        block.removeAttribute('x');
-        block.removeAttribute('y');
-      }
-    }
-    text = Blockly.Xml.domToText(xml);
-  
-  return text;
-}
-// import { useEffect } from 'react';
-// import * as Blockly from 'blockly';
-// import { javascriptGenerator } from 'blockly/javascript';
-// import { /*save,*/ load } from './serialization';
-// import './index.css';
-// import {Container} from '@mui/material';
-// import Interpreter from 'js-interpreter';
-// import {initInterpreter} from './Interpreter.js';
-
-
-
-// import { toolboxXML, Blocks } from './Blocks';
-
-// // Set up UI elements and inject Blockly
-
-// export default function HU01() {
-//   Blocks();
-//   useEffect(() => {
-//     const blocklyDiv = document.getElementById('blockly');
-
-//     // Inicializa los bloques y generadores
-//     // Blocks();
-    
-
-//     // Configurar elementos de la UI y inyectar Blockly
-//     const ws = Blockly.inject(blocklyDiv, { toolbox : toolboxXML });
-//     console.log("Workspace inicializado:\n",ws);
-
-//     const codeDiv = document.getElementById('generatedCode').firstChild;
-//     const outputDiv = document.getElementById('output');
-    
-
-//     // Función para resetear los divs de código y output, mostrar el código generado y ejecutarlo
-//     const runCode = () => {
-//       console.log("Workspace cambio o se genero:\n",ws);
-//       const code = javascriptGenerator.workspaceToCode(ws);
-//       console.log("Codigo generado:\n",code);
-//       const interpreter = new Interpreter(code, initInterpreter);
-//       console.log("Interpretador Generado:\n",interpreter);
-//       codeDiv.innerText = code; // Comentar esta línea para mostrar el código generado
-//       if (code!='') {
-//         outputDiv.innerHTML = '';
-//         eval(code);
-//       }
-//     };
-
-//     // Cargar el estado inicial desde el almacenamiento y ejecutar el código
-//     load(ws);
-//     runCode();
-
-//     // Guardar los cambios en el almacenamiento cada vez que el workspace cambie de estado
-//     ws.addChangeListener((e) => {
-//       if (e.isUiEvent) return;
-//       // save(ws);
-//     });
-
-//     // Ejecutar el código cada vez que el workspace cambie significativamente
-//     ws.addChangeListener((e) => {
-//       if (e.isUiEvent || e.type == Blockly.Events.FINISHED_LOADING || ws.isDragging()) {
-//         return;
-//       }
-//       runCode();
-//     });
-//   }, []);
-
-//   return (
-//     <div id="pageContainer">
-//       <Container id="outputPane">
-//         <pre id="generatedCode"><code></code></pre>
-//         <Container id="output"/>
-//       </Container>
-//       <div id="blockly"/>
-//     </div>
-//   );
-// }
